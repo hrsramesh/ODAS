9211bd08ccb36d6b207553e18b94f82ceea6696c
sagina <saginapraj@outlook.com>
2014-08-30 21:36:25 +0545

    Revert "Revert "Revert "Revert "This is the code by Sagina""""
    This reverts commit 5688869d2498fb2165db4b67af69ac942dbd3e2b.

---
+++ Doctor/DoctorRegistration.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Security.Cryptography;
+using System.IO;
+using System.Text;
+
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnReg_Click(object sender, EventArgs e)
+    {
+        AddUser.AddDoctor(txtName.Text, txtAddress.Text, txtEmail.Text, txtUserName.Text, Encrypt(txtPass.Text.Trim()), Convert.ToInt32(txtNmcReg.Text), Convert.ToInt32(txtCharge.Text), DdlGender.SelectedItem.Text);
+        string display = "inserted successfully";
+        ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + display + "');", true);
+
+        txtName.Text = "";
+        txtEmail.Text = "";
+        txtUserName.Text = "";
+        txtAddress.Text = "";
+        txtPass.Text = "";
+        txtConfirmPass.Text = "";
+       
+        DdlGender.SelectedItem.Text = "";
+        txtNmcReg.Text = "";
+        txtCharge.Text = "";
+
+//        SqlConnection con = new SqlConnection(AppConfig.DBConnectionString);
+//        SqlCommand cmd = new SqlCommand("insert into tbl_Doctor(Doc_Name,Doc_Address,Doc_Email,Doc_UserName,Doc_Password,Doc_NMC_Reg,Doc_Charges,Doc_Gender) values(@name,@address,@email,@userName,@password,@nmcReg,@charge,@gender)", con);
+//        cmd.Parameters.AddWithValue("@name", txtName.Text);
+//        cmd.Parameters.AddWithValue("@address", txtAddress.Text);
+       
+//        cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+//        cmd.Parameters.AddWithValue("@userName", txtUserName.Text);
+//        cmd.Parameters.AddWithValue("@password", txtPass.Text);
+//        cmd.Parameters.AddWithValue("@nmcReg", Convert.ToInt32(txtNmcReg.Text));
+//        cmd.Parameters.AddWithValue("@charge",Convert.ToInt32(txtCharge.Text));
+//        cmd.Parameters.AddWithValue("@gender", Convert.ToString(DdlGender.SelectedValue));
+    
+//        con.Open();
+//        int rowsAffected = cmd.ExecuteNonQuery();
+       
+//        if (rowsAffected == 1)
+//        {
+//            string script = @"<script language=""javascript"">
+//        alert('Information have been Saved Successfully.......!!!!!.');
+//       </script>;";
+//            Page.ClientScript.RegisterStartupScript(this.GetType(), "myJScript1", script);
+
+//        }
+//        else
+//        {
+//            string script = @"<script language=""javascript"">
+//        alert('Error');
+//       </script>;";
+//            Page.ClientScript.RegisterStartupScript(this.GetType(), "myJScript1", script);
+//        }
+//        con.Close();
+
+    }
+  public string Encrypt(string clearText)
+    {
+        string EncryptionKey = "MAKV2SPBNI99212";
+        byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+        using (Aes encryptor = Aes.Create())
+        {
+            Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+            encryptor.Key = pdb.GetBytes(32);
+            encryptor.IV = pdb.GetBytes(16);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(clearBytes, 0, clearBytes.Length);
+                    cs.Close();
+                }
+                clearText = Convert.ToBase64String(ms.ToArray());
+            }
+        }
+        return clearText;
+    }
+}
---
+++ WebUserControl/DocProControl.ascx.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+public partial class UserControl_DocProControl : System.Web.UI.UserControl
+{
+   
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+         
+        //       lblSpec.Visible = false;
+        //       lblDegree.Visible = false;
+        //       lblCharge.Visible = false;
+        //       lblUni.Visible = false;
+        //ddlSpec.Visible = false;
+        //txtDegree.Visible = false;
+        //txtUniversity.Visible = false;
+        //txtCharge.Visible = false;
+        //btnSubmit.Visible = false;
+       
+
+            SqlConnection con = new SqlConnection(AppConfig.DBConnectionString);
+            SqlCommand cmd = new SqlCommand("select * from tbl_Specialization", con);
+            con.Open();
+            ddlSpec.DataSource = cmd.ExecuteReader();
+
+            ddlSpec.DataTextField = "Sp_Area";
+            ddlSpec.DataValueField = "Sp_Id";
+            ddlSpec.DataBind();
+            con.Close();
+
+        
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (Session["uname"] != null)
+        {
+           
+                SqlConnection con1 = new SqlConnection(AppConfig.DBConnectionString);
+                SqlCommand cmd1 = new SqlCommand("insert into tbl_DocSpec(Doc_Specialization,University,Degree,Doc_Charges,U_Name)values(@spec,@university,@degree,@charge,@uname)", con1);
+                // SqlCommand cmd1 = new SqlCommand("update tbl_Doctor set Doc_Specialization=@spec,University=@university,Degree=@degree where U_Name='" + Session["uname"] + "'");
+                cmd1.Parameters.AddWithValue("@spec", ddlSpec.SelectedItem.Text);
+                cmd1.Parameters.AddWithValue("@university", txtUniversity.Text);
+                cmd1.Parameters.AddWithValue("@degree", txtDegree.Text);
+                cmd1.Parameters.AddWithValue("@charge", txtCharge.Text);
+                cmd1.Parameters.AddWithValue("@uname", Session["uname"].ToString());
+                con1.Open();
+                int n = cmd1.ExecuteNonQuery();
+                if (n > 0)
+                {
+                    string script = @"<script language=""javascript"">
+      alert('Information have been Saved Successfully.......!!!!!.');
+     </script>;";
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "myJScript1", script);
+
+                }
+                else
+                {
+                    string script = @"<script language=""javascript"">
+                alert('Error');
+               </script>;";
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "myJScript1", script);
+
+                }
+                con1.Close();
+            }
+        }
+
+    protected void txtSpec_TextChanged(object sender, EventArgs e)
+    {
+        if (Session["uname"] != null)
+        {
+
+            SqlConnection con1 = new SqlConnection(AppConfig.DBConnectionString);
+            SqlCommand cmd = new SqlCommand("insert into tbl_Specialization(Title)values(@title)", con1);
+            cmd.Parameters.AddWithValue("@title", txtSpec.Text);
+            con1.Open();
+            cmd.ExecuteNonQuery();
+            con1.Close();
+
+            ddlSpec.Items.Add(txtSpec.Text);
+        }
+    }
+}
+    
+       
+
+
+        
+
+    
+   
